f477cc5a4ec3ccd8da03f400f21b7b4172b4eede
Mila B <milabochkova03@gmail.com>
2022-04-17 10:06:46 +0300

    Initial Commit

---
+++ Shared/NLPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPAppApp.swift
+//  Shared
+//
+//  Created by Mila B on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NLPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
